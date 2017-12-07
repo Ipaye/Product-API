84bd1481fd0ed887eb03937aa15439bf043e0c53
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-12-07 14:43:57 +0100

    Implement error handling

--- app.js
+++ app.js
@@ -8,4 +8,19 @@ app.use(require('morgan')('dev'))
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error("End-point not found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
 module.exports = app;
