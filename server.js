1190b5bac178aa01c1a60bca04a7a008b4d2bbeb
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-12-27 17:12:44 +0100

    Beautified terminal output

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log('App listening on Port ' + port);
+	console.log('App listening on Port http://localhost:' + port);
 });
