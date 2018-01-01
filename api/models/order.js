ec2f6a09d466d328261c119cfe26ae7d21598c99
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-01-01 21:37:42 +0100

    Update Rrequire Message Error output

--- api/models/order.js
+++ api/models/order.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const orderSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	quantity: { type: Number, default: 1 },
-	productID: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }
+	productID: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Product',
+		required: 'Please pass in a product ID'
+	}
 });
 
 module.exports = mongoose.model('Order', orderSchema);
--- api/models/product.js
+++ api/models/product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
 	_id: mongoose.SchemaTypes.ObjectId,
-	name: {type:String, required: true},
+	name: { type: String, required: 'Name is required' },
 	price: Number
 });
 
