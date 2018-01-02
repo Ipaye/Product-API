4d828b1c25fb1b127f6bb5ac6b9a6ff0fde0dca9
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-01-02 21:46:42 +0100

    Add file Upload to Product Route

--- api/models/product.js
+++ api/models/product.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
 	_id: mongoose.SchemaTypes.ObjectId,
 	name: { type: String, required: 'Name is required' },
-	price: Number
+	price: Number,
+	productImage: { type: String, required: 'Product Image is Required' }
 });
 
 module.exports = mongoose.model('Product', productSchema);
