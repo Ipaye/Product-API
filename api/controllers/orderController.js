64e0f87a17a4cfa5e09871225b4a11825299ef5d
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-01-06 22:31:09 +0100

    Finish Order route middleware refactor

---
+++ api/controllers/orderController.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+const Product = require('../models/product');
+
+exports.getAllOrders = (req, res, next) => {
+	Order.find()
+		//This populates the Product Field and the select only the field specified in the second argument
+		.populate('productID', '_id name price')
+		.then(orders => {
+			console.log(orders);
+			res.status(200).json({
+				status: 'success',
+				message: 'Getting All Orders',
+				orders: orders.map(order => {
+					return {
+						_id: order._id,
+						quantity: order.quantity,
+						product: order.productID,
+						request: {
+							type: 'GET',
+							url: 'http://localhost:3000/orders/' + order._id
+						}
+					};
+				}),
+				count: orders.length
+			});
+		})
+		.catch(err => {
+			next(err);
+		});
+};
+
+exports.createOrder = (req, res, next) => {
+	const newOrder = new Order({
+		_id: mongoose.Types.ObjectId(),
+		productID: req.body.productID,
+		quantity: req.body.quantity
+	});
+
+	const pid = req.body.productID;
+	console.log('pid', pid, 'newOrder', newOrder);
+	Product.findById(pid)
+		.exec()
+		.then(product => {
+			if (product) {
+				newOrder.save().then(result => {
+					console.log(result);
+					return res.status(201).json({
+						message: 'Saved new Order 🙋',
+						status: 'Success',
+						result
+					});
+				});
+			} else {
+				res.status(422).json({
+					status: 'failed',
+					message: 'Unable to Post, Check form data'
+				});
+			}
+		})
+		.catch(err => {
+			next(err);
+		});
+};
+
+exports.getSingleOrder = (req, res, next) => {
+	let orderID = req.params.id;
+	Order.findById(orderID)
+		.populate('productID', '_id name price')
+		.select('_id productId quantity')
+		.exec()
+		.then(result => {
+			res.status(200).json({
+				_id: result._id,
+				quantitiy: result.quantity,
+				productDetails: result.productID,
+				Request: {
+					type: 'DELETE/PATCH',
+					url: `http://localhost:3000/orders/${result._id}`
+				}
+			});
+		})
+		.catch(err => {
+			next(err);
+		});
+};
+
+exports.deleteOrder = (req, res, next) => {
+	let orderID = req.params.id;
+	Order.findByIdAndRemove(orderID)
+		.then(result => {
+			if (!result) {
+				return res.status(404).json({
+					status: 'Failed',
+					message: 'invalid Delete Request'
+				});
+			}
+			res.status(200).json({
+				status: 'success',
+				result,
+				message: 'Order Deleted',
+				Request: {
+					Type: 'Post',
+					url: 'http://localhost:3000/orders'
+				}
+			});
+		})
+		.catch(err => {
+			next(err);
+		});
+};
